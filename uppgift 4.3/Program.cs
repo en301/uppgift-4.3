01f76eeea64f568e94047eec184ff0a6ff0c4aca
en301 <emilsjostrand1000@gmail.com>
2023-10-18 10:34:21 +0200

    Add project files.

---
+++ uppgift 4.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace uppgift_4_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 0;
+            while (tal <= 100)
+            {
+
+                if (tal % 3 == 0 && tal % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    tal++;
+                }
+                else if (tal % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    tal++;
+                }
+                else if (tal % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    tal++;
+                }
+                else
+                {
+                    Console.WriteLine(tal);
+                    tal++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
